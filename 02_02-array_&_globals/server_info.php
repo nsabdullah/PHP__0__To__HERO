60da131e25983fe4f6e448a2f7a787a7f7575ed5
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-22 17:09:19 +0600

    super globals done

---
+++ 02_02-array_&_globals/server_info.php
@@ -0,0 +1,21 @@
+<?php
+
+$server_info = $_SERVER;
+echo '<pre>';
+// print_r($server_info);
+
+
+
+$globals = $GLOBALS;
+
+// print_r($GLOBALS);
+
+$get = $_GET;
+print_r($get);
+
+
+
+
+
+// http://localhost/PHP__0__To__HERO/02_02-array_&_globals/server_info.php
+?>
