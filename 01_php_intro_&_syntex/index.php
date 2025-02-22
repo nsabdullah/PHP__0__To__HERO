e8aff4c7f3f05f02f0dc479b7e371c79ea43fcfa
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-22 11:15:52 +0600

    complete

---
+++ 01_php_intro_&_syntex/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "My first PHP script!" . "<br>";
+echo "I am a developer";
+
+
+
+// http://localhost/PHP__0__To__HERO/01_php_intro_&_syntex/
+?>
---
+++ 03_variable_&_data_types/index.php
@@ -0,0 +1,26 @@
+<?php
+// PHP 
+//PHP data types
+//PHP Error
+
+
+$userName = "Jhon";
+  echo $userName;
+
+$name = "errror";
+  echo  $name ;
+
+
+
+$String = "This is a ecommerce web site";
+$Integer = " Pure number without dot(.) as like 1,2,11,22,34,354325,235425";
+$Float = "duble number with dot(.) as like 1.2, 3.4, 22.1, 345.23";
+$Boolean = "True/False";
+$Array = array("Volvo","BMW","Toyota");
+
+print_r($Array);
+var_dump($Array);
+  
+
+// http://localhost/PHP__0__To__HERO/03_variable_&_data_types
+?>
