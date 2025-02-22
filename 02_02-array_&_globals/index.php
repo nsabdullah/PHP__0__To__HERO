dd4d1933d4a18cfc0257c4257e0b2dea93534dd8
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-22 16:41:07 +0600

    Array complete

---
+++ 02_02-array_&_globals/index.php
@@ -0,0 +1,44 @@
+<?php
+
+// Array
+// Associative Array 
+// Super bGlobals Variable
+$students = array("Amin", "jabir", "Fasir");
+echo "<pre>";
+// print_r($students);
+// var_dump($students);
+$studentS = array(
+            'one' => array('Abdullah', 'Abdur Rahman', 'Abdur Razzaq', 'Abul Hasan', 'Adam'),
+            'two' => array('Ahmed', 'Akram', 'Ali', 'Amir', 'Anas', 'Arafat', 'Arman', 'Asad', 'Ashfaq', 'Asif'),
+            'three' => array('Burhan', 'Dawood', 'Ebrahim', 'Elias', 'Faheem', 'Faisal', 'Farhan'),
+            'four' => array('Farid', 'Faysal', 'Firoz', 'Ghulam', 'Habib', 'Hakeem', 'Hamid'),
+            'five' => array('Haris', 'Hasan', 'Hashim', 'Hossain', 'Humayun', 'Ibrahim', 'Idris'),
+            'six' => array('Imran', 'Inam', 'Iqbal', 'Irfan', 'Isa', 'Ismail', 'Jafar', 'Jahid', 'Jalal'),
+            'seven' => array('Jamil', 'Junaid', 'Kabir', 'Kamal', 'Karim', 'Khalid', 'Latif', 'Luqman'),
+            'eight' => array('Mahbub', 'Mahdi', 'Mahfuz', 'Mahir', 'Mamun', 'Mansur', 'Maruf')
+            
+            );
+// print_r($studentS);
+
+$number = array(1, 2, 3, 4, 5, 6, 7, 8);
+// print_r($number);
+
+
+
+
+$numbers = array(
+             'one' => 'Abdullah',
+             'two' => 'Abdur Rahman',
+             'three' => 'Abdur Razzaq',
+             'four' => 'Abul Hasan',
+             'five' => 'Adam',
+             'six' => 'Ahmed',
+             'seven' => 'Akram',
+             'eight' => 'Ali',
+             'nine' => 'Amir',
+             'ten' => 'Anas'
+            );
+print_r($numbers);
+
+// http://localhost/PHP__0__To__HERO/02_02-array_&_globals
+?>
