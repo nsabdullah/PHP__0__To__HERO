72bfb57233ab23d088b45cc57adf4c7904d6b9e3
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-23 17:16:47 +0600

    loggin page

---
+++ 06_condition_&_layout/loggin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
+
+<h1>Please logg in </h1>
+
+
+<!-- http://localhost/PHP__0__To__HERO/06_condition_&_layout/loggin.php -->
+
+
+
+
+</body>
+</html>
