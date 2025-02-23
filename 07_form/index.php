13afb8dc7570e3ba50b0966357ca0ea7a8440f7f
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-23 17:22:05 +0600

    form

---
+++ 07_form/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+
+<form action="form_submit.php" method="get">
+  <input type="text" name="" id=""> <br>
+  <input type="text" name="" id=""><br>
+  <input type="file" name="" id=""><br>
+  <input type="submit" name="" id="">
+  
+</form>
+
+
+
+
+<!-- // http://localhost/PHP__0__To__HERO/07_form -->
+</body>
+</html>
