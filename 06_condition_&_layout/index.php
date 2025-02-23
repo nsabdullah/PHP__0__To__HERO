3e5549a939a7a571dbf3affbc2738fe23f93e015
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-02-23 17:16:29 +0600

    index

---
+++ 06_condition_&_layout/index.php
@@ -0,0 +1,62 @@
+<?php
+$userage = 20;
+
+/*
+if ($userage == 20){
+  echo "You are able";
+}else{
+  echo "You are not";
+} 
+
+*/
+
+
+/*
+if($userage == 23){
+  echo "Your age is 20";
+
+}elseif($userage == 21){
+  echo "Your age is 21";
+
+}elseif($userage == 22){
+  echo "Your age is 22";
+
+}elseif($userage == 23){
+  echo "Your age is 23";
+
+}else{
+ echo  'You are not';
+}
+*/
+// Multiple value chack
+
+/*
+if ($userage > 20 || $userage < 30 ){
+  echo "You are accepted";
+}
+*/
+/*
+$isloggedin = true;
+if(!$isloggedin){
+  echo "You are logged in";
+  echo "<button>Logout</button>";
+}else{
+  echo  "You are logged out. ";
+  echo "<button>Login</button>";
+}
+*/
+
+ // include
+ include('layout/header.php');
+ echo "<h1>Home Page content </h1>";
+ include('layout/footer.php');
+
+
+
+
+
+
+
+
+// http://localhost/PHP__0__To__HERO/06_condition_&_layout
+?>
