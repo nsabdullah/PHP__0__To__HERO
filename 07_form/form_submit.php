d6e4d4af0f603f791dedc3e0740b43beb174c06d
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-05-05 10:46:44 +0600

    form

---
+++ 07_form/form_submit.php
@@ -0,0 +1,10 @@
+<?php
+echo "<pre>";
+$requesr_mathod = $_REQUEST;
+
+ echo $requesr_mathod["username"];      
+ echo $requesr_mathod["email"];
+
+
+
+?>
--- 07_form/index.php
+++ 07_form/index.php
@@ -1,3 +1,6 @@
+<!-- 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,6 +23,27 @@
 
 
 
-<!-- // http://localhost/PHP__0__To__HERO/07_form -->
-</body>
+  // http://localhost/PHP__0__To__HERO/07_form -->
+<!-- </body>
 </html>
+  -->
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    
+    <form action="form_submit.php">
+      
+      <input type="text" name = "username"> <br> <br>
+      <input type="text" name = "email"><br><br>
+      <input type="submit">  
+ 
+    </form>
+
+  </body>
+  </html>
